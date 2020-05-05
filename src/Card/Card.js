a84b21bc6bde234dd4b8295e3e80e8d7c3cff180
pakayb <pakayb@gmail.com>
2020-05-05 14:17:49 +0200

    Add flipping on click to cards

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -4,23 +4,39 @@ import {storage} from "../firebase";
 
 function Card(props) {
     const [image, setImage] = useState("");
-    let storageRef = storage.ref();
-    if (props.picture!=undefined){let pictureUrl = props.picture;
-    let downloadUrl = storage.refFromURL(pictureUrl).getDownloadURL().then(url=> setImage(url));}
+    if (props.picture!==undefined){let pictureUrl = props.picture;
+    storage.refFromURL(pictureUrl).getDownloadURL().then(url=> setImage(url));}
+
+    const flip = (event) => {
+        console.log('asd');
+        let element = event.currentTarget;
+        console.log(element);
+        if (element.className ==="card"){
+            if (element.style.transform=="rotateY(180deg)"){element.style.transform = "rotateY(0deg)"}
+            else{element.style.transform = "rotateY(180deg)"}
+        }
+    };
+
+
+
     return (
-        <div className='card text-center'>
-            <div className='overflow'>
+        <div className='container'>
+            <div className='card' onClick={flip}>
+        <div className=' text-center' >
+            <div className='overflow front'>
                 <img id={props.id} alt={props.id} src={image} className='card-img-top'></img>
             </div>
-            <div className='card-body text-dark'>
+            <div className='card-body text-dark back'>
                 <h4 className='card-title'>{props.name}</h4>
-                <p className='card-text text-secondary'>
+                <div className='card-text text-secondary'>
                     <p>Role: {props.role}</p>
                     <p>Location: {props.location}</p>
                     <p>Gender: {props.gender}</p>
                     <p>Funfact: {props.funFact}</p>
                     <p>Has twin? {props.hasTwin}</p>
-                </p>
+                </div>
+            </div>
+        </div>
             </div>
         </div>
     );
